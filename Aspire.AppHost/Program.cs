350406ce3d9ce62c7aa813554b75793046656be3
jlabclouds <jlabcloud@outlook.com>
2025-07-05 21:10:43 -0400

    update host program.cs with db and kafka connectors create kafkaclient.cs

--- Aspire.AppHost/Program.cs
+++ Aspire.AppHost/Program.cs
@@ -8,3 +8,31 @@ builder.AddProject<Projects.Aspire_Web>("webfrontend")
     .WaitFor(apiService);
 
 builder.Build().Run();
+
+
+/* 
+* This code sets up a distributed application using the Aspire framework.
+ * It creates a builder for the application, adds an API service project,
+ * and then adds a web frontend project that references the API service.
+ * The web frontend is configured to wait for the API service to be ready
+ * before starting.
+ */
+
+ /** Call AddOracle to add and return an Oracle server resource 
+ builder. Chain a call to the returned resource builder to AddDatabase,
+ to add an Oracle database to the server resource:
+ **/
+// var oracleServer = builder.AddOracle().AddDatabase("mydb");
+// If you'd rather connect to an existing Oracle server, call AddConnectionString 
+var builder = DistributedApplication.CreateBuilder(args);
+
+var oracle = builder.AddOracle("oracle, password")
+                    .WithLifetime(ContainerLifetime.Persistent);
+
+var oracledb = oracle.AddDatabase("oracledb");
+
+builder.AddProject<Projects.Aspire_#>()
+       .WithReference(oracledb);
+       .WaitFor(oracledb);
+
+// After adding all resources, run the app...
